5f1afe7678149025c7a0bb1c4f6b7a19af465cb7
Florin <83332450+flawreen@users.noreply.github.com>
2023-02-27 22:11:02 +0200

    Update lab_bd.sh

--- lab_bd.sh
+++ lab_bd.sh
@@ -4,4 +4,5 @@ dir_name="lab$1"
 path="$HOME/DataGripProjects/BD"
 firefox https://teams.microsoft.com/l/channel/19%3a9Sbj4xl4wzxyVb1QkOzMjmRmWYOCCDwh0_awqBj4N041%40thread.tacv2/General?groupId=087ee5d2-0375-498b-835b-0b303048e2f1&tenantId=08a1a72f-fecd-4dae-8cec-471a2fb7c2f1 &
 mkdir $path/$dir_name
+touch $path/"$dir_name.sql"
 datagrip $path &
